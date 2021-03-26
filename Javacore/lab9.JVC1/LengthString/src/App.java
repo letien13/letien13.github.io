3aea4f8b2ba20cd5d9cf629ce5bdddae930c6a77
Tien <lequyettien93@gmail.com>
2021-03-26 15:38:35 +0700

    add Javacore

--- Javacore/lab9.JVC1/LengthString/src/App.java
+++ Javacore/lab9.JVC1/LengthString/src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập chuỗi: ");
+        String chuoi = scanner.nextLine();
+        System.out.println("Kích thước của chuỗi: " + chuoi.length());
     }
 }
